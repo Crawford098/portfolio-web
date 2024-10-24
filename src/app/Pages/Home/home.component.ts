14405060a6be7438fa7be0bd1b1ebecd947f18dc
Jonathan <jonathan@Jonathans-iMac.local>
2024-10-24 16:12:02 -0400

    package prices created an showed in protfolio

--- src/app/Pages/Home/home.component.ts
+++ src/app/Pages/Home/home.component.ts
@@ -2,15 +2,25 @@ import { Component } from '@angular/core';
 import { CoverComponent } from '../../components/cover/cover.component';
 import { NavBarComponent } from '../../components/navBar/navBar.component';
 import { NavBarItems } from '../../components/navBar/navBarItems/navBarItems.component';
+import WordpressPackage from '../../../data/WordpressPackage.json';
+import { NgFor } from '@angular/common';
 
 @Component({
     selector: 'home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    imports: [CoverComponent, NavBarComponent, NavBarItems]
+    imports: [CoverComponent, NavBarComponent, NavBarItems, NgFor]
 })
 
 export class HomeComponent {
 
+    wordPressPackage : any = WordpressPackage;
+    columnKeys: string[] = [];
+
+    ngOnInit(): void {
+        this.columnKeys = Object.keys(this.wordPressPackage[0].columns);
+        console.log(this.wordPressPackage);
+        console.log(this.columnKeys);
+      }
 }
